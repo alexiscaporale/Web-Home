abc867477efcbf20090a874ddc59a379b94ecf6d
alexiscaporale <60515416+alexiscaporale@users.noreply.github.com>
2026-03-24 15:19:59 +0100

    Update Logo.tsx

--- Solander898v2/src/app/components/Logo.tsx
+++ Solander898v2/src/app/components/Logo.tsx
@@ -25,7 +25,7 @@ export function Logo({
   className = "",
 }: LogoProps) {
   const dim = {
-    sm: { image: 60, wm: 11,  gap: 9,  track: 3   },
+    sm: { image: 40, wm: 11,  gap: 9,  track: 3   },
     md: { image: 80, wm: 13,  gap: 11, track: 3.5 },
     lg: { image: 120, wm: 17,  gap: 16, track: 4   },
   }[size];
