a3dc36ee124f64c3aed54d0d5f293117742f36c3
alexiscaporale <60515416+alexiscaporale@users.noreply.github.com>
2026-03-25 12:37:00 +0000

    Cambios

--- Solander898v2/src/main.tsx
+++ Solander898v2/src/main.tsx
@@ -2,6 +2,5 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
-
+  console.log("Versión nueva: " + Date.now());
   createRoot(document.getElementById("root")!).render(<App />);
-  
